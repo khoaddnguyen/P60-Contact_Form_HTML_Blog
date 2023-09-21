c7dd6f4fe5ef08fe1f1d7f94f190fe4caaa8e208
kwa_laa <khoaddnguyen@gmail.com>
2023-09-21 13:19:24 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import requests
+
+BLOG_API_URL = "https://api.npoint.io/eb6cd8a5d783f501ee7d"
+posts = requests.get(BLOG_API_URL).json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/form-entry", methods=["POST"])
+def receive_data():
+    data = request.form
+    print(data["name"])
+    print(data["email"])
+    print(data["phone"])
+    print(data["message"])
+    return "<h1>Successfully sent your message</h1>"
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
