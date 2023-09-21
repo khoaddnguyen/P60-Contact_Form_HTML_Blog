246107135c58b9269eb942012a55b36cb7aed0c8
kwa_laa <khoaddnguyen@gmail.com>
2023-09-21 13:44:50 -0500

    display Success Message when info is submitted

--- main.py
+++ main.py
@@ -38,8 +38,9 @@ def contact():
         print(data["email"])
         print(data["phone"])
         print(data["message"])
-        return "<h1>Successfully sent your message</h1>"
-    return render_template("contact.html")
+        # return "<h1>Successfully sent your message</h1>"
+        return render_template("contact.html", message_sent=True)
+    return render_template("contact.html", message_sent=False)
 
 @app.route("/post/<int:index>")
 def show_post(index):
